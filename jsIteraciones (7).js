c04d59991123f9282f7e1a2323d63e6e93e1bd05
Ignacio <47308121+orellanaignacio@users.noreply.github.com>
2019-02-25 12:36:18 -0300

    Add files via upload
    progresp

---
+++ jsIteraciones (7).js
@@ -0,0 +1,14 @@
+function Mostrar()
+{
+
+	var contador=0;
+	var acumulador=0;
+	var numero;
+	var respuesta;
+
+	
+
+document.getElementById('suma').value=acumulador;
+document.getElementById('promedio').value= promedio;
+
+}//FIN DE LA FUNCIÓN
